cde66c938212b40a585c1bd49dd18d378c6cbb67
Jinesh15 <159221181+Jinesh15@users.noreply.github.com>
2024-02-11 14:38:29 +0530

    Add files via upload

---
+++ promise.js
@@ -0,0 +1,39 @@
+let clickCount = 0;
+
+function noFunc() {
+    const messages = [
+        "You can't say no to me",
+        "Just Click the Yes Button",
+        "Are you sure you want to say No?",
+        "Think again, its very important",
+        "You are stubborn just click YES!!",
+        "Why are you saying No, please think of me",
+        "You won't get this opportunity again",
+        "I am giving you a last chance, Say YES!!!",
+        "Okay fine, now you have no option, you're stuck"
+    ];
+
+    if (clickCount < messages.length - 1) {
+        const selectedMessage = messages[clickCount];
+        document.getElementById("text").innerText = selectedMessage;
+        const currentSize = window.getComputedStyle(document.getElementById("yesButton")).getPropertyValue('font-size');
+        const newSize = parseFloat(currentSize)*1.5;
+        document.getElementById("yesButton").style.fontSize = newSize+"px"
+    }
+    else {
+        const selectedMessage = messages[clickCount];
+        document.getElementById("text").innerText = selectedMessage;
+        document.getElementById("noButton").remove();
+        const currentSize = window.getComputedStyle(document.getElementById("yesButton")).getPropertyValue('font-size');
+        const newSize = parseFloat(currentSize)*1.5;
+        document.getElementById("yesButton").style.fontSize = newSize+"px"
+    }
+    clickCount++;
+}
+
+function yesFunc() {
+    document.getElementById("text").innerText = "Thank You for finally agreeing with Me\n I Love you too❤️";
+    document.getElementById("noButton").remove();
+    document.getElementById("yesButton").remove();
+    document.getElementById("image").classList.remove("image");    
+}
